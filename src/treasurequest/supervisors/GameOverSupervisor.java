fa3ce1589b39d5684f0848b1e84533a5ed492f2f
Q210115 <alex.tadino@student.helmo.be>
2023-05-21 16:57:02 +0200

    CTT Calculed

--- src/treasurequest/supervisors/GameOverSupervisor.java
+++ src/treasurequest/supervisors/GameOverSupervisor.java
@@ -12,8 +12,28 @@ import java.time.Duration;
  * Réagit aux événements de haut-niveau de sa vue et lui fournit des données à afficher.
  *
  * ITERATION 3
- * CONDITIONS :
+ * CONDITIONS DE FIN DE PARTIE :
  * - L'ensemble des trésors a été trouvé
+ * - Le joueur a fait banqueroute si :
+ * le montant du joueur est égal à 0
+ * l'ensemble des cases non-creusées ont un montant inférieur au montant du joueur
+ *
+ * CTT POUR DETERMINER LA PLUS GRANDE ZONE CREUSEE PAR LE JOUEUR : (Se situe dans la classe ProfilPlayerFactory avec la méthode biggerZone)
+ * Une première boucle va boucler sur l'ensemble des cases de la carte. Cette méthode est en O(n) avec n l'ensemble des coordonnées creusées par le joueur
+ * Dans cette boucle :
+ * - La méthode contains est en O(1)
+ * - La méthode compareTo est en O(1)
+ * - La méthode exploreZone qui :
+ * - add la cordonnée de la case courante est en O(1)
+ * Une deuxième boucle qui va boucler sur l'ensemble de coordonnées en O(n - 1) avec n l'ensemble des coordonnées creusées par le joueur et -1 la case courante
+ * Dans cette boucle :
+ * - La méthode isAdjacent est en O(1)
+ * - La méthode get est en O(1)
+ * - La méthode getCaseType est en O(1)
+ * - Un appel récursif à la méthode exploreZone qui est en O(n - 1) avec n (dans le pire des cas si l'ensemble des cases creusés par le joeuur ne sont pas adjacente) l'ensemble des coordonnées creusées par le joueur et -1 la case courante
+ * - La méthode fusionateAt est en O(1)
+ *
+ * Pour résumer la complexité de la méthode biggerZone est en O(n^3) avec n l'ensemble des coordonnées creusées par le joueur
  * */
 public class GameOverSupervisor {
 	private final GameFactory gameFactory;
@@ -27,6 +47,7 @@ public class GameOverSupervisor {
 	public GameOverSupervisor(GameFactory gameFactory) {
 		this.gameFactory = gameFactory;
 	}
+
 	public void setView(GameOverView view) {
 		if(view == null) {
 			return;
